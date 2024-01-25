4189a9af5e58bae9733d11b938b75b80999e56b7
Roman <romazelenskiy2000@gmail.com>
2024-01-25 16:50:59 +0200

    fix

--- components/sections/Reviews.vue
+++ components/sections/Reviews.vue
@@ -1,29 +1,29 @@
 <script setup lang="ts">
   const reviews: any[] = [
     {
-      name: 'Emily Thompson',
+      name: 'Natalia',
       review:
-        "I couldn't be happier with the website that Johnson Web Studios created for my online boutique. They truly understood my vision and translated it into a stunning, user-friendly site. The team was professional, responsive, and delivered beyond my expectations. I highly recommend Johnson Web Studios for anyone looking for top-notch web development",
+        "Working with Ocean Script was a game-changer for our business. Their web development expertise brought our online presence to a whole new level. The team's dedication and creativity truly set them apart.",
     },
     {
-      name: 'Andrew Davis',
+      name: 'Dmitri',
       review:
-        'Working with Smith Design Co. was a fantastic experience. From the initial consultation to the final launch, their team demonstrated creativity, technical expertise, and a keen attention to detail. The website they built for my consulting business not only looks sleek but also functions seamlessly. I appreciate their dedication to delivering a product that aligns perfectly with my brand.',
+        'Impressed with the seamless collaboration and top-notch results delivered by Ocean Script. Their web solutions are not just functional, but they also have an eye for design that adds a touch of elegance to the user experience. Highly recommended!',
     },
     {
-      name: 'Olivia Carter',
+      name: 'Andrei',
       review:
-        'Hats off to Miller Web Solutions for bringing my e-commerce vision to life! The team was not only professional and knowledgeable but also incredibly patient with my numerous requests. They were attentive to every detail and offered valuable insights throughout the development process. The result? A visually appealing and high-performing website that has significantly boosted my online sales.',
+        'Ocean Script turned our vision into reality with their impeccable web development skills. The team was responsive, professional, and went above and beyond to ensure our website exceeded expectations. A reliable partner for any online venture.',
     },
     {
-      name: 'Liam Johnson',
+      name: 'Aleksander',
       review:
-        "I approached Thompson Web Designs with a complex project, and I'm thoroughly impressed with the outcome. Their technical prowess and problem-solving skills were evident from the start. The communication was excellent, and they went above and beyond to meet tight deadlines. Thompson Web Designs is my go-to recommendation for anyone seeking a reliable and skilled web development partner.",
+        'Partnering with Ocean Script was a breeze from start to finish. Their expertise in web development is matched by their commitment to client satisfaction. Our website is now a powerful tool thanks to their innovative approach and attention to detail.',
     },
     {
-      name: 'Ella Reynolds',
+      name: 'Jaroslav',
       review:
-        "Harrison Creative Studios turned my concept into a digital masterpiece. Their team's creativity and innovation shine through in every aspect of my website. Not only did they deliver a visually stunning site, but they also ensured it was highly functional and easy to navigate. I'm grateful for their commitment to excellence and the positive impact it's had on my online presence.",
+        'Thrilled with the results of our collaboration with Ocean Script. Their web development team demonstrated a deep understanding of our industry, translating complex ideas into a user-friendly website. Their professionalism and commitment to quality are commendable.',
     },
   ];
   const reviewsSection = ref();
@@ -60,7 +60,7 @@
           :fill="'column'"
           :rows="2"
         >
-          <SwiperSlide v-for="(review, index) in reviews" :key="index" class=" self-stretch">
+          <SwiperSlide v-for="(review, index) in reviews" :key="index" class="self-stretch">
             <CommonReviewItem :review="review" class="!h-full" />
           </SwiperSlide>
         </Swiper>
