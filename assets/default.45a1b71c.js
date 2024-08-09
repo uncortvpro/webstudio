fa0650c4dda5d70819d3cd990874f21e29eea244
roman-zelenskiy <73440148+roman-zelenskiy@users.noreply.github.com>
2024-08-09 23:09:37 +0000

    Deploying to gh-pages from @ uncortvpro/webstudio@79d3a8ff6713d4eec3183a8e18777e721ce13c93 🚀

---
+++ assets/default.45a1b71c.js
@@ -0,0 +1,1 @@
+import{_,x as e}from"./entry.de1d1df0.js";import{a}from"./NuxtScrollbar.3a5ce761.js";import{B as c,C as n,A as r,D as s}from"./swiper-vue.a3ca0911.js";const m={};function f(p,x){const t=e,o=a;return r(),c(o,{tag:"div",class:"max-h-[100svh]"},{default:n(()=>[s(t)]),_:1})}const i=_(m,[["render",f]]);export{i as default};
