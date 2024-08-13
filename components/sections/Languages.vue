7b9be8de278ef9ba53f9a9eaedb946de00509a74
Andrey <uncortvpro@gmail.com>
2024-08-13 22:55:02 +0200

    Refactor tree

--- components/sections/Languages.vue
+++ components/sections/Languages.vue
@@ -1,30 +1,33 @@
-<script setup></script>
+<script setup>
+  const logos = [
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/Logo.min.svg/1280px-Logo.min.svg.png',
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/1184px-Vue.js_Logo_2.svg.png',
+    'https://upload.wikimedia.org/wikipedia/de/d/dd/MySQL_logo.svg',
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/PHP-logo.svg/711px-PHP-logo.svg.png?20180502235434',
+    'https://upload.wikimedia.org/wikipedia/commons/7/70/Docker_logo.png',
+    'https://www.xpand-it.com/wp-content/uploads/2018/09/React-Logo.png',
+    'https://seeklogo.com/images/N/nuxt-logo-5EF50E1ABD-seeklogo.com.png',
+    'https://go.dev/blog/go-brand/Go-Logo/PNG/Go-Logo_Blue.png',
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/2048px-Typescript_logo_2020.svg.png',
+    'https://seeklogo.com/images/M/mongodb-logo-D13D67C930-seeklogo.com.png',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTeKPw4CK4jcH7udsFHZdiB3iIOuI3fUCsxUZosXy4Y1yd25NA-dzCBPrSDIhg1BwObl3w&usqp=CAU',
+    'https://marketplacedesignoye.s3.ap-south-1.amazonaws.com/postgresql-branding-icon-symbol-logo-vector-.png',
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ee/Redis_logo.svg/2560px-Redis_logo.svg.png',
+    'https://miro.medium.com/v2/resize:fit:800/1*v2vdfKqD4MtmTSgNP0o5cg.png',
+    'https://book.git-scm.com/images/logos/downloads/Git-Icon-1788C.png',
+    'https://static-00.iconduck.com/assets.00/neo4j-icon-452x512-b63ajo4a.png',
+    'https://upload.wikimedia.org/wikipedia/commons/a/ad/Figma-1-logo.png',
+    'https://cdn.pixabay.com/photo/2017/08/05/11/16/logo-2582748_1280.png',
+    'https://cdn.pixabay.com/photo/2017/08/05/11/16/logo-2582747_1280.png',
+    'https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png',
+    'https://upload.wikimedia.org/wikipedia/commons/b/b2/Bootstrap_logo.svg',
+  ];
+</script>
 
 <template>
   <Vue3Marquee>
-    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/Logo.min.svg/1280px-Logo.min.svg.png" alt="Logo 1" class="mx-4 h-12" />
-    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/1184px-Vue.js_Logo_2.svg.png" alt="Logo 1" class="mx-4 h-12" />
-    <img src="https://upload.wikimedia.org/wikipedia/de/d/dd/MySQL_logo.svg" alt="Logo 1" class="mx-4 h-12" />
-    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/PHP-logo.svg/711px-PHP-logo.svg.png?20180502235434" alt="Logo 1" class="mx-4 h-12" />
-    <img src="https://upload.wikimedia.org/wikipedia/commons/7/70/Docker_logo.png" alt="Logo 1" class="mx-4 h-12" />
-    <img src="https://www.xpand-it.com/wp-content/uploads/2018/09/React-Logo.png" alt="Logo 1" class="mx-4 h-12" />
-    <img src="https://seeklogo.com/images/N/nuxt-logo-5EF50E1ABD-seeklogo.com.png" alt="Logo 1" class="mx-4 h-12" />
-    <img src="https://go.dev/blog/go-brand/Go-Logo/PNG/Go-Logo_Blue.png" alt="Logo 1" class="mx-4 h-12" />
-    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/2048px-Typescript_logo_2020.svg.png" alt="Logo 1" class="mx-4 h-12" />
-    <img src="https://seeklogo.com/images/M/mongodb-logo-D13D67C930-seeklogo.com.png" alt="Logo 1" class="mx-4 h-12" />
-    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTeKPw4CK4jcH7udsFHZdiB3iIOuI3fUCsxUZosXy4Y1yd25NA-dzCBPrSDIhg1BwObl3w&usqp=CAU" alt="Logo 1" class="mx-4 h-12" />
-    <img src="https://marketplacedesignoye.s3.ap-south-1.amazonaws.com/postgresql-branding-icon-symbol-logo-vector-.png" alt="Logo 1" class="mx-4 h-12" />
-    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ee/Redis_logo.svg/2560px-Redis_logo.svg.png" alt="Logo 1" class="mx-4 h-12" />
-    <img src="https://miro.medium.com/v2/resize:fit:800/1*v2vdfKqD4MtmTSgNP0o5cg.png" alt="Logo 1" class="mx-4 h-12" />
-    <img src="https://book.git-scm.com/images/logos/downloads/Git-Icon-1788C.png" alt="Logo 1" class="mx-4 h-12" />
-    <img src="https://static-00.iconduck.com/assets.00/neo4j-icon-452x512-b63ajo4a.png" alt="Logo 1" class="mx-4 h-12" />
-    <img src="https://upload.wikimedia.org/wikipedia/commons/a/ad/Figma-1-logo.png" alt="Logo 1" class="mx-4 h-12" />
-    <img src="https://cdn.pixabay.com/photo/2017/08/05/11/16/logo-2582748_1280.png" alt="Logo 1" class="mx-4 h-12" />
-    <img src="https://cdn.pixabay.com/photo/2017/08/05/11/16/logo-2582747_1280.png" alt="Logo 1" class="mx-4 h-12" />
-    <img src="https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png" alt="Logo 1" class="mx-4 h-12" />
-    <img src="https://upload.wikimedia.org/wikipedia/commons/b/b2/Bootstrap_logo.svg" alt="Logo 1" class="mx-4 h-12" />
+    <img v-for="logo in logos" :src="logo" alt="Logo 1" class="mx-4 h-12" />
   </Vue3Marquee>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
