418bb34380459867545856abcb486dfcaa0dfabe
Andrey <uncortvpro@gmail.com>
2024-08-25 23:29:55 +0200

    Change margins

--- pages/index.vue
+++ pages/index.vue
@@ -6,13 +6,13 @@
     <div class="relative overflow-hidden">
 
       <SectionsBestProjects id="best_projects" />
-      <SectionsServices id="services" class="my-[70px] md:my-[95px] xl:my-[120px]" />
+      <SectionsServices id="services" class="my-[40px] md:my-[95px] xl:my-[120px]" />
 
       <SectionsNews />
-      <SectionsLanguages class="mt-[70px] md:mt-[95px] xl:mt-[120px]" />
+      <SectionsLanguages class="mt-[40px] md:mt-[95px] xl:mt-[120px]" />
 
-      <SectionsWhyWe class="mt-[70px] md:mt-[95px] xl:mt-[120px]" />
-      <SectionsContact id="contact" class="relative z-10 mt-[70px] xl:mt-[120px]" />
+      <SectionsWhyWe class="mt-[40px] md:mt-[95px] xl:mt-[120px]" />
+      <SectionsContact id="contact" class="relative z-10 mt-[40px] xl:mt-[120px]" />
       <LayoutFooter class="relative" />
     </div>
   </div>
