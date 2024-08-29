88d6d987c169918c1994ddd3b85a7c60374284c5
Roman <romazelenskiy2000@gmail.com>
2024-08-29 13:35:43 +0300

    add news

--- components/common/CardNews.vue
+++ components/common/CardNews.vue
@@ -12,7 +12,13 @@
 <template>
   <UiButton class="flex flex-col items-stretch justify-between" @click="switchModalDetails(true)">
     <div class="flex flex-col items-stretch">
-      <img class="rounded-[20px] border border-black" loading="lazy" :src="useBaseUrl(news?.image)" />
+      <div class="relative pt-[65%]">
+        <img
+          class="absolute left-0 top-0 size-full rounded-[20px]"
+          loading="lazy"
+          :src="useBaseUrl(news?.image)"
+        />
+      </div>
       <h3
         class="mt-[15px] text-left text-[15px] text-black md:text-[18px] xl:mt-[20px] xl:text-[22px]"
       >
@@ -32,14 +38,14 @@
     :ui="{ container: 'items-center', width: '!max-w-[1480px]' }"
     @closeModal="switchModalDetails(false)"
   >
-    <div class="flex flex-col gap-[15px] md:flex-row xl:gap-[20px]">
+    <div class="flex flex-col gap-[15px] pb-[20px] md:flex-row xl:gap-[20px]">
       <img class="md:w-full md:max-w-[555px]" :src="useBaseUrl(news?.image)" />
       <div class="mt-[15px] lg:mt-0">
         <h3 class="text-[20px] uppercase text-black md:text-[30px] xl:text-[45px]">
           {{ news.title }}
         </h3>
         <p
-          class="mt-[5px] font-secondary text-[10px] leading-[130%] text-black md:mt-[10px] md:text-[14px] xl:mt-[20px] xl:text-[20px]"
+          class="mt-[5px] text-[10px] leading-[130%] text-black md:mt-[10px] md:text-[14px] xl:mt-[20px] xl:text-[20px]"
         >
           {{ news.description }}
         </p>
