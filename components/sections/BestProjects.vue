3da43f3bf22f4d564cdc0a1aa3705cd3be596ceb
Roman <romazelenskiy2000@gmail.com>
2024-09-01 21:27:26 +0300

    fix padding rescent project

--- components/sections/BestProjects.vue
+++ components/sections/BestProjects.vue
@@ -120,10 +120,10 @@
       v-show="isShowSection"
       class="duration_show_section relative animate-fade-up bg-[rgba(141,122,100,0.1)] py-[25px] md:py-[35px] xl:py-[44px]"
     >
-      <UiSection class="relative">
+      <UiSection class="relative" :ui="{ container: 'pb-0 xl:pb-[20px]' }">
         <template #title> recent projects </template>
       </UiSection>
-      <div class="3xl:container mt-0">
+      <div class="mt-0 3xl:container">
         <Swiper
           class="swiper_tabs_button w-full"
           :slides-per-view="'auto'"
