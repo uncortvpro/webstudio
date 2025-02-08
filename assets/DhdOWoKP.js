639e1a5e09cc62d25d4c41d4d20893ca7b431f2f
uncortvpro <90911508+uncortvpro@users.noreply.github.com>
2025-02-08 00:40:31 +0000

    Deploying to gh-pages from @ uncortvpro/webstudio@494f84788b740826e874a122299b10d0a4d31a95 🚀

--- assets/DhdOWoKP.js
+++
@@ -1,1 +0,0 @@
-import{N as r}from"./D9pLcs2B.js";import{h as e,v as t,x as n,y as s}from"./19l7XzjW.js";/* empty css        */const a={xmlns:"http://www.w3.org/2000/svg",fill:"none",viewBox:"0 0 10 6"};function l(p,o){return t(),n("svg",a,o[0]||(o[0]=[s("path",{d:"M9 5 5 1 1 5"},null,-1)]))}const w={render(){return e(r,{icon:{render:l},name:"ArrowTop"})}};export{w as default,l as render};
